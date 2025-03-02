116c78f53af34a52bfad81704a57463e8f224f40
Soto <rodrigo.soto@airesdigital.com.br>
2025-03-02 00:34:21 -0300

    Tratamento para campos vazios

--- src/App.js
+++ src/App.js
@@ -118,13 +118,13 @@ function adicionaLinha(idTabela, id, ticker, variacao, vlrAtual, vlrAbertura, vl
     }
   } else if (existRow) {
     var linha = document.getElementById(id);
-    const atualizacaoAnt= linha.childNodes[8].innerHTML.toString();
+    const atualizacaoAnt= linha.childNodes[8]!=null?linha.childNodes[8].innerHTML.toString():'';
     // const isDateAnt = isData(atualizacaoAnt)
     // const isDateAtu = isData(atualizacao)
 
     // alert("isDateAnt: "+isDateAnt);
     // alert("isDateAtu: "+isDateAtu);
-    // alert("atualizacaoAnt: "+atualizacaoAnt);
+    // alert("Ação: "+linha.childNodes[1].innerHTML.toString()+", atualizacaoAnt: "+atualizacaoAnt+", atualizacao: "+atualizacao);
     // alert("atualizacao: "+atualizacao);
     // alert("atualizacaoAnt.length: "+atualizacaoAnt.length);
     // alert("atualizacao.length: "+atualizacao.length);
@@ -133,71 +133,84 @@ function adicionaLinha(idTabela, id, ticker, variacao, vlrAtual, vlrAbertura, vl
     const timeAnt = moment(atualizacaoAnt, "HH:mm");
     const timeAtu = moment(atualizacao, "HH:mm");
 
-    if ((timeAtu > timeAnt) || (atualizacaoAnt.length > 1 && atualizacao.length === 0) || (atualizacaoAnt.length === 0 && atualizacao.length > 1)) {
-      const vAntVar = Number.parseFloat(linha.childNodes[2].innerHTML.replace(',','.')).toPrecision(4);
-      const vAtuVar = Number.parseFloat(variacao.replace(',','.')).toPrecision(4) ;
-      const vAntAtu = Number.parseFloat(linha.childNodes[3].innerHTML.replace(',','.')).toPrecision(4);
-      const vAtuAtu= Number.parseFloat(vlrAtual.replace(',','.')).toPrecision(4) ;
-      const vAntAbe = Number.parseFloat(linha.childNodes[4].innerHTML.replace(',','.')).toPrecision(4);
-      const vAtuAbe= Number.parseFloat(vlrAbertura.replace(',','.')).toPrecision(4) ;
-      const vAntMai = Number.parseFloat(linha.childNodes[5].innerHTML.replace(',','.')).toPrecision(4);
-      const vAtuMai= Number.parseFloat(vlrMaior.replace(',','.')).toPrecision(4) ;
-      const vAntMen = Number.parseFloat(linha.childNodes[6].innerHTML.replace(',','.')).toPrecision(4);
-      const vAtuMen= Number.parseFloat(vlrMenor.replace(',','.')).toPrecision(4) ;
-  
-  
-      const objValVar = document.getElementById('celVariacao'+'_'+id);
-      if(vAtuVar > 0) {
-        objValVar.style.color ='rgb(34,139,34)';
-      }else if(vAtuVar == 0){
-        objValVar.style.color ='rgb(0,206,209)';
-      }else if (vAtuVar < 0) {
-        objValVar.style.color ='rgb(165,42,42)';
+    // if(atualizacao === null){
+    //   alert("atualizacao: "+atualizacao+", timeAtu: "+timeAtu);
+    //   atualizacao='';
+
+    // }
+
+    // if(atualizacaoAnt === null){
+    //   alert("atualizacaoAnt: "+atualizacaoAnt+", timeAnt: "+timeAnt);
+    // }
+    
+    if(atualizacao != null && atualizacaoAnt != null && timeAnt != NaN && timeAtu != NaN) {
+      // alert("atualizacao: "+atualizacao+", timeAtu: "+timeAtu+"atualizacaoAnt: "+atualizacaoAnt+", timeAnt: "+timeAnt);
+      if ((timeAtu > timeAnt) || (atualizacaoAnt.length > 1 && atualizacao.length === 0) || (atualizacaoAnt.length === 0 && atualizacao.length > 1)) {
+        const vAntVar = Number.parseFloat(linha.childNodes[2] === null ? '' : linha.childNodes[2].innerHTML.replace(',','.')).toPrecision(4);
+        const vAtuVar = Number.parseFloat(variacao === null ? '' : variacao.replace(',','.')).toPrecision(4) ;
+        const vAntAtu = Number.parseFloat(linha.childNodes[3] === null ? '' : linha.childNodes[3].innerHTML.replace(',','.')).toPrecision(4);
+        const vAtuAtu= Number.parseFloat(vlrAtual=== null ? '' :vlrAtual.replace(',','.')).toPrecision(4) ;
+        const vAntAbe = Number.parseFloat(linha.childNodes[4] === null ? '' : linha.childNodes[4].innerHTML.replace(',','.')).toPrecision(4);
+        const vAtuAbe= Number.parseFloat(vlrAbertura=== null ? '' :vlrAbertura.replace(',','.')).toPrecision(4) ;
+        const vAntMai = Number.parseFloat(linha.childNodes[5] === null ? '' : linha.childNodes[5].innerHTML.replace(',','.')).toPrecision(4);
+        const vAtuMai= Number.parseFloat(vlrMaior=== null ? '' :vlrMaior.replace(',','.')).toPrecision(4) ;
+        const vAntMen = Number.parseFloat(linha.childNodes[6] === null ? '' : linha.childNodes[6].innerHTML.replace(',','.')).toPrecision(4);
+        const vAtuMen= Number.parseFloat(vlrMenor=== null ? '' :vlrMenor.replace(',','.')).toPrecision(4) ;
+    
+    
+        const objValVar = document.getElementById('celVariacao'+'_'+id);
+        if(vAtuVar > 0) {
+          objValVar.style.color ='rgb(34,139,34)';
+        }else if(vAtuVar == 0){
+          objValVar.style.color ='rgb(0,206,209)';
+        }else if (vAtuVar < 0) {
+          objValVar.style.color ='rgb(165,42,42)';
+        }
+    
+        const objValAtu = document.getElementById('celVlrAtual'+'_'+id);
+        if(vAtuAtu > vAntAtu) {
+          objValAtu.style.color ='rgb(34,139,34)';
+        }else if( (vAtuAtu == vAntAtu) && (objValAtu.style.color != 'rgb(34, 139, 34)' && objValAtu.style.color != 'rgb(165, 42, 42)')){
+          objValAtu.style.color ='rgb(0,206,209)';
+        }else if (vAtuAtu < vAntAtu) {
+          objValAtu.style.color ='rgb(165,42,42)';
+        }
+    
+        const objValAbe = document.getElementById('celVlrAbertura'+'_'+id);
+        if(vAtuAbe > vAntAbe) {
+          objValAbe.style.color ='rgb(34,139,34)';
+        }else if( (vAtuAbe == vAntAbe) && (objValAbe.style.color != 'rgb(34, 139, 34)' && objValAbe.style.color != 'rgb(165, 42, 42)')){
+          objValAbe.style.color ='rgb(0,206,209)';
+        }else if (vAtuAbe < vAntAbe) {
+          objValAbe.style.color ='rgb(165,42,42)';
+        }
+    
+        const objValMai = document.getElementById('celVlrMaior'+'_'+id);
+        if(vAtuMai > vAntMai) {
+          objValMai.style.color ='rgb(34,139,34)';
+        }else if( (vAtuMai == vAntMai) && (objValMai.style.color != 'rgb(34, 139, 34)' && objValMai.style.color != 'rgb(165, 42, 42)')){
+          objValMai.style.color ='rgb(0,206,209)';
+        }else if (vAtuMai < vAntMai) {
+          objValMai.style.color ='rgb(165,42,42)';
+        }
+    
+        const objValMen = document.getElementById('celVlrMenor'+'_'+id);
+        if(vAtuMen > vAntMen) {
+          objValMen.style.color ='rgb(34,139,34)';
+        }else if( (vAtuMen == vAntMen) && (objValMen.style.color != 'rgb(34, 139, 34)' && objValMen.style.color != 'rgb(165, 42, 42)')){
+          objValMen.style.color ='rgb(0,206,209)';
+        }else if (vAtuMen < vAntMen) {
+          objValMen.style.color ='rgb(165,42,42)';
+        }
+    
+        linha.childNodes[2].innerHTML =  variacao;
+        linha.childNodes[3].innerHTML =  vlrAtual;
+        linha.childNodes[4].innerHTML = vlrAbertura;
+        linha.childNodes[5].innerHTML =  vlrMaior;
+        linha.childNodes[6].innerHTML =  vlrMenor;
+        linha.childNodes[7].innerHTML =  status;
+        linha.childNodes[8].innerHTML =  atualizacao;
       }
-  
-      const objValAtu = document.getElementById('celVlrAtual'+'_'+id);
-      if(vAtuAtu > vAntAtu) {
-        objValAtu.style.color ='rgb(34,139,34)';
-      }else if( (vAtuAtu == vAntAtu) && (objValAtu.style.color != 'rgb(34, 139, 34)' && objValAtu.style.color != 'rgb(165, 42, 42)')){
-        objValAtu.style.color ='rgb(0,206,209)';
-      }else if (vAtuAtu < vAntAtu) {
-        objValAtu.style.color ='rgb(165,42,42)';
-      }
-  
-      const objValAbe = document.getElementById('celVlrAbertura'+'_'+id);
-      if(vAtuAbe > vAntAbe) {
-        objValAbe.style.color ='rgb(34,139,34)';
-      }else if( (vAtuAbe == vAntAbe) && (objValAbe.style.color != 'rgb(34, 139, 34)' && objValAbe.style.color != 'rgb(165, 42, 42)')){
-        objValAbe.style.color ='rgb(0,206,209)';
-      }else if (vAtuAbe < vAntAbe) {
-        objValAbe.style.color ='rgb(165,42,42)';
-      }
-  
-      const objValMai = document.getElementById('celVlrMaior'+'_'+id);
-      if(vAtuMai > vAntMai) {
-        objValMai.style.color ='rgb(34,139,34)';
-      }else if( (vAtuMai == vAntMai) && (objValMai.style.color != 'rgb(34, 139, 34)' && objValMai.style.color != 'rgb(165, 42, 42)')){
-        objValMai.style.color ='rgb(0,206,209)';
-      }else if (vAtuMai < vAntMai) {
-        objValMai.style.color ='rgb(165,42,42)';
-      }
-  
-      const objValMen = document.getElementById('celVlrMenor'+'_'+id);
-      if(vAtuMen > vAntMen) {
-        objValMen.style.color ='rgb(34,139,34)';
-      }else if( (vAtuMen == vAntMen) && (objValMen.style.color != 'rgb(34, 139, 34)' && objValMen.style.color != 'rgb(165, 42, 42)')){
-        objValMen.style.color ='rgb(0,206,209)';
-      }else if (vAtuMen < vAntMen) {
-        objValMen.style.color ='rgb(165,42,42)';
-      }
-  
-      linha.childNodes[2].innerHTML =  variacao;
-      linha.childNodes[3].innerHTML =  vlrAtual;
-      linha.childNodes[4].innerHTML = vlrAbertura;
-      linha.childNodes[5].innerHTML =  vlrMaior;
-      linha.childNodes[6].innerHTML =  vlrMenor;
-      linha.childNodes[7].innerHTML =  status;
-      linha.childNodes[8].innerHTML =  atualizacao;
     }
   }
 }
